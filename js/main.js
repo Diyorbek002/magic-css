9fc7fe8210874b9bde4e15f8b62cd8df0a94a488
Diyorbek002 <zamonbekovdiyorbek98@gamil.com>
2022-02-18 22:59:49 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var isNpm = prompt ("npm ni toliq nomi nima?");
+var connectionPaths = prompt ("bootstrapni ulashni necha xil yoli bor va ularni sanab bering?");
+var imageFormat = prompt ("rasmni webp formatga o'girilganda va responsiv qilinganda rasm necha xil formatda bo'ladi?");
+var isNodejs = prompt ("nodejs nima?");
+var namingInProgramming = prompt ("dasturlashda nomlash turlarini davom ettiring PascalCase ...");
+var isBootstrap = prompt ("bootstrap nima uchun kerak?");
+var isAdaptation = prompt ("adoptive bilan responsive ning farqi nimada?");
+var isGitHub = prompt ("git hub nima?");
+var isDiffers = prompt ("sass va scss ni farqi nimada?");
+var terminalProcess = prompt ("terminalda push qilishgacha va bo'lgan jarayonni aytib bering");
+
+console.log(isNpm + "\n" + connectionPaths + "\n"
+ + imageFormat + "\n" + isNodejs + "\n" + namingInProgramming 
+ + "\n" + isBootstrap + "\n" + isAdaptation + "\n" + isGitHub + "\n" 
+ + isDiffers + "\n" + terminalProcess);
