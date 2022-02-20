22d54d77af0e2c33a0926e34fdeaf359a5916048
Diyorbek002 <zamonbekovdiyorbek98@gamil.com>
2022-02-20 11:41:11 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-var isNpm = prompt ("npm ni toliq nomi nima?");
-var connectionPaths = prompt ("bootstrapni ulashni necha xil yoli bor va ularni sanab bering?");
-var imageFormat = prompt ("bootstrap necha xil rang bor?");
-var isNodejs = prompt ("nodejs nima?");
-var namingInProgramming = prompt ("dasturlashda nomlash turlarini davom ettiring PascalCase ...");
-var isBootstrap = prompt ("bootstrap nima uchun kerak?");
-var isAdaptation = prompt ("breackpointlarni sanab bering?");
-var isGitHub = prompt ("git hub nima?");
-var isDiffers = prompt ("alert nima?");
-var terminalProcess = prompt ("bootstrapda inputni class nima?");
+// var isNpm = prompt ("npm ni toliq nomi nima?");
+// var connectionPaths = prompt ("bootstrapni ulashni necha xil yoli bor va ularni sanab bering?");
+// var imageFormat = prompt ("bootstrap necha xil rang bor?");
+// var isNodejs = prompt ("nodejs nima?");
+// var namingInProgramming = prompt ("dasturlashda nomlash turlarini davom ettiring PascalCase ...");
+// var isBootstrap = prompt ("bootstrap nima uchun kerak?");
+// var isAdaptation = prompt ("breackpointlarni sanab bering?");
+// var isGitHub = prompt ("git hub nima?");
+// var isDiffers = prompt ("alert nima?");
+// var terminalProcess = prompt ("bootstrapda inputni class nima?");
 
-console.log (isNpm + "\n" + connectionPaths + "\n"
- + imageFormat + "\n" + isNodejs + "\n" + namingInProgramming 
- + "\n" + isBootstrap + "\n" + isAdaptation + "\n" + isGitHub + "\n" 
- + isDiffers + "\n" + terminalProcess);
+// console.log (isNpm + "\n" + connectionPaths + "\n"
+//  + imageFormat + "\n" + isNodejs + "\n" + namingInProgramming 
+//  + "\n" + isBootstrap + "\n" + isAdaptation + "\n" + isGitHub + "\n" 
+//  + isDiffers + "\n" + terminalProcess);
+
+var result = Number ( prompt (`Summani kiriting` , 9648000));
+
+if(result >= 9648000){
+    console.log("Oq yo'l, Alisher");
+}else if(result <= 9648000){
+    console.log("Brat ketomisz soqqani qb keyin kelin");
+}
